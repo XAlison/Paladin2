a85a316db0bd39236534f86589f99d4221c090d9
听风 <511693597@qq.com>
2019-01-07 17:11:25 +0800

    修正mp生成器参数

--- paladin2-backend/paladin2-generator/src/main/java/MybatisPlusGenerator.java
+++ paladin2-backend/paladin2-generator/src/main/java/MybatisPlusGenerator.java
@@ -16,20 +16,24 @@ import java.io.IOException;
  */
 public class MybatisPlusGenerator {
     // 数据库相关设置
-    // TODO 需要調整
     private static final String DB_HOST = "localhost";
     private static final String DB_PORT = "3306";
-    private static final String DB_NAME = "hd_xgx";
+    private static final String DB_NAME = "paladin2";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root";
     // 指定需要生成的表名（没有指定，则全部生成）
-    // TODO 需要調整
     private static String[] tables = new String[]{
-            "sys_admin"
+           "sys_admin",
+           "sys_admin_role",
+           "sys_permission",
+           "sys_permission_resource",
+           "sys_role",
+           "sys_role_permission",
     };
     // 指定生成的目录（以整个工程的根目录为准）
-    // TODO 需要調整
-    private static final String OUT_PATH = "/paladin2-common/src/main/java";
+    private static final String OUT_PATH = "/paladin2-core/src/main/java";
+    // 指定生成的目录（以整个工程的根目录为准）
+    private static final String PACKAGE_NAME = "org.zhangxiao.paladin2.core";
 
     /**
      * <p>
@@ -106,7 +110,7 @@ public class MybatisPlusGenerator {
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setParent("org.zhangxiao.paladin2.common");
+        pc.setParent(PACKAGE_NAME);
         mpg.setPackageInfo(pc);
 
         //不生成 Controller
